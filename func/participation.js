1436895dea0149503db3fb95f164807dfed72809
SusanElizabeth <susan.smith1024@gmail.com>
2016-09-22 20:11:19 -0500

    master

--- func/participation.js
+++ func/participation.js
@@ -1,8 +1,13 @@
 'use strict';
 
-module.exports.main = function(user, messages, callback) {
-    var response = {
-        message: 'Participation handler function'
+module.exports.main = function(user, history, callback) {
+    var response = 0, count = 0, total = 0;
+    while (history){
+      if (history.get('userId') == userId){
+        count++;
+      }
+      total++;
     }
+    response = ((count/total) * 100).toPercent();
     callback(null, response);
 };

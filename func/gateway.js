817087930218d776afb408655f38474d506a34cc
Adam Smith <Adam.Smith@lifeway.com>
2016-09-23 17:37:11 -0500

    Invoking child lambda functions

--- func/gateway.js
+++ func/gateway.js
@@ -1,16 +1,118 @@
 'use strict';
 
 var Slack = require('slack-node');
+var AWS = require('aws-sdk');
+var Async = require('async');
 var Config = require('../lib/config.js');
 
 module.exports.main = function(event, context, callback) {
-    var slack = new Slack(Config.devToken);
+    if (!event.body.channel_id) {
+        var err = "Could not retrieve channel id from event";
+        console.error(err)
+        callback(err);
+        return;
+    }
 
-    slack.api(Config.channelHistoryEndpoint, {channel: event.body.channel_id}, function(err, data) {
-        if (err) {
-          console.error(err);
-          callback(err);
-        }
-        callback(null, data);
-    });
+    if (!event.body.user_id) {
+        var err = "Could not retrieve user id from event";
+        console.error(err)
+        callback(err);
+        return;
+    }
+
+    var userId = event.body.user_id;
+
+    Async.series([
+            function(cb) {
+                var slack = new Slack(Config.devToken);
+                var params = {
+                    channel: event.body.channel_id
+                };
+                slack.api(Config.channelHistoryEndpoint, params, function(err, data) {
+                    if (err || data.ok === 'true') {
+                        var error = "Error calling Slack API for channel history - " + err;
+                        console.error(error);
+                        cb(error);
+                        return;
+                    }
+                    cb(null, data.messages);
+                });
+            }
+        ],
+        function(err, data) {
+            if (err) {
+                console.error(err);
+                callback(err);
+                return;
+            }
+
+            var arns = Config.getArns(event.stage);
+            var lambda = new AWS.Lambda();
+            var payload = JSON.stringify({
+                userId: event.body.user_id,
+                messages: data[0]
+            });
+
+            Async.parallel({
+                    participation: function(cb) {
+                        var params = {
+                            FunctionName: arns.participation,
+                            Payload: payload,
+                        };
+                        lambda.invoke(params, function(error, response) {
+                            if (error) {
+                                console.error("Error invoking participation function - " + error)
+                                cb(error);
+                                return;
+                            }
+
+                            callback(null, response);
+                        });
+                    },
+                    positivity: function(cb) {
+                        var params = {
+                            FunctionName: arns.positivity,
+                            Payload: payload,
+                        };
+                        lambda.invoke(params, function(error, response) {
+                            if (error) {
+                                console.error("Error invoking positivity function - " + error)
+                                cb(error);
+                                return;
+                            }
+
+                            callback(null, response);
+                        });
+                    },
+                    conceited: function(cb) {
+                        var params = {
+                            FunctionName: arns.conceited,
+                            Payload: payload,
+                        };
+                        lambda.invoke(params, function(error, conceited) {
+                            if (error) {
+                                console.error("Error invoking participation function - " + error)
+                                cb(error);
+                                return;
+                            }
+
+                            callback(null, response);
+                        });
+                    }
+                },
+                function(error, results) {
+                    if (error) {
+                        console.error("Error invoking child lambda function - " + error)
+                        callback(error);
+                        return;
+                    }
+
+                    console.log(results);
+
+                    callback(null, {
+                        response: results
+                    });
+                });
+
+        });
 };
--- handler.js
+++ handler.js
@@ -2,20 +2,44 @@
 
 var Functions = require('require-directory')(module, './func');
 
-module.exports.gateway = function(event, context, cb) {
+module.exports.gateway = function(event, context, callback) {
     Functions.gateway.main(event, context, function(err, data) {
-        cb(null, "true");
+        if (err) {
+            callback(null, {
+                text: "Oops, something went wrong"
+            });
+            return;
+        }
+        callback(null, "gateway");
     });
 };
 
-module.exports.participation = function(event, context, cb) {
+module.exports.participation = function(event, context, callback) {
     Functions.participation.main(event, context, function(err, data) {
-        cb(null, data);
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, "participation");
     });
 };
 
-module.exports.positivity = function(event, context, cb) {
+module.exports.positivity = function(event, context, callback) {
     Functions.positivity.main(event, context, function(err, data) {
-        cb(null, data);
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, "positivity");
+    });
+};
+
+module.exports.conceited = function(event, context, callback) {
+    Functions.conceited.main(event, context, function(err, data) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, "conceited");
     });
 };
